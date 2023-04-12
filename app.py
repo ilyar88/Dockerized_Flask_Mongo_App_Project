04de5263a3ffb72d1bc56c2f1b3160a4ef5e54e8
ilya-rahmilevich <56227367+ilya-rahmilevich@users.noreply.github.com>
2023-04-12 14:43:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import json
-from flask import Flask, jsonify, request
+from flask import Flask, Blueprint, jsonify, render_template, request
 from tmdbv3api import TMDb
 from tmdbv3api import Movie
 tmdb=TMDb()
 from pymongo import MongoClient
 
-app = Flask(__name__)
+app_blueprint = Blueprint('app_blueprint',__name__)
 
 def get_db():
     client = MongoClient(host='test_mongodb',
@@ -16,27 +16,29 @@ def get_db():
     db = client["movie_db"]
     return db
 
-@app.route('/search')
-def search():
+
+
+@app_blueprint.route('/')
+def index():
     query = request.args.get('query')
     TMDb.api_key=''
     tmdb.language = 'en'
     tmdb.debug = True
     movie=Movie()
     similar = movie.search(query)
-    dictionary = { "poster_path" : stu.poster_path for stu in similar } 
+    dictionary = { "poster_path" : query + ": " + stu.poster_path for stu in similar } 
     with open('sample.json', 'w') as file:
      file.write(json.dumps(dictionary))
-    return "Welcome!"
+    return render_template("index.html",Search=query)
 
-@app.route('/remove')
+@app_blueprint.route('/remove')
 def remove():
     query = request.args.get('query')
     db = get_db()
     Collection = db["movie_db"]
     Collection.delete_one(query)
 
-@app.route('/movies')
+@app_blueprint.route('/movies')
 def movies():
     db = get_db()
     with open('sample.json') as file:
@@ -47,10 +49,7 @@ def movies():
     collection.insert_one(file_data)
     _movie = db.movie_db.find()
     movie = [{"poster_path": m["poster_path"]} for m in _movie]
-    return jsonify({"poster_path": movie})
+    return jsonify({"Movies": movie})
     
-if __name__=='__main__':
-    app.run(host="0.0.0.0",debug=True, port=5000)
-
 
 
