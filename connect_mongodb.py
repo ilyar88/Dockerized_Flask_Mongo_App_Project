7ec1e3ff200032b536db0674200fc7ce8a8dc5dc
ilya-rahmilevich <56227367+ilya-rahmilevich@users.noreply.github.com>
2023-04-15 21:20:56 +0300

    Update connect_mongodb.py

--- connect_mongodb.py
+++ connect_mongodb.py
@@ -1,12 +1,15 @@
+from dotenv import load_dotenv
 from pymongo import MongoClient
+import os
 
 class connect_mongodb():
 
     def get_db():
+        load_dotenv()
         client = MongoClient(host='test_mongodb',
         port=27017, 
-        username='root', 
-        password='pass',
+        username = os.getenv("USERNAME"), 
+        password = os.getenv("PASSWORD"),
         authSource="admin")
         db = client["movie_db"]
         return db
